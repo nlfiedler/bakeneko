6c076dcf8fa39d43a2d6dc6c9ad017299135b7c5
Nathan Fiedler <nlfiedler@yahoo.com>
2013-03-30 17:43:51 -0700

    Adding a main.go for compilation purposes

---
+++ main.go
@@ -0,0 +1,16 @@
+//
+// Copyright 2013 Nathan Fiedler. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+// Package bakeneko implements the Scheme REPL.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Thanks for trying bakeneko, currently doesn't do anything.")
+}
